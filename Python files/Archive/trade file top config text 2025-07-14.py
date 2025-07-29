3d79b2d940da68f6a8c577541a8dde00cef5c98b
gmafanasiev <gmafanasiev@gmail.com>
2025-07-29 11:24:28 -0400

    deleted

--- Python files/Archive/trade file top config text 2025-07-14.py
+++
@@ -1,9 +0,0 @@
-# Configure logging
-logging.basicConfig(filename='/home/gmafanasiev/trading.log', level=logging.INFO, format='%(asctime)s - %(message)s')
-
-# Schwab and xAI API credentials
-SCHWAB_APP_KEY = "5AqBefcOK5NJbuv0twAo6s41Ulc2cRlr"
-SCHWAB_APP_SECRET = "o76s0GYzvACjHO1e"
-SCHWAB_CALLBACK_URL = "https://127.0.0.1:8182"
-XAI_API_KEY = "xai-BU2X5OnnITYxBZN5Q3dc8mNrbyw4RrxdrM7caEZvaDZJpASF0e8v6S6IX6HLlQ98WLCvlsK5Ik2HLGhr"
-TOKEN_PATH = "/home/gmafanasiev/token.json"
--- Python files/Testing scripts, logs/test_logging.py
+++
@@ -1,6 +0,0 @@
-# test_logging.py
-from logging_utils import setup_logging
-logger, file_handler = setup_logging('/home/gmafanasiev/ai_trading_bot/test_log.log')
-logger.info("Test log message")
-file_handler.flush()
-print("Logging setup completed")
